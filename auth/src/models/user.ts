801206e24481b9781d89044ca30cfb6778098d8d
ttyt0416 <ttyt0416@gmail.com>
2021-03-17 16:55:55 +0900

    finishing sign up service with jwt wrapped with cookie

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -27,6 +27,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            ret.id = ret._id;
+            delete ret.id;
+            delete ret.password;
+            delete ret.__v;
+        }
+    }
 });
 
 userSchema.pre('save', async function(done) {
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response} from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -35,7 +36,19 @@ async (req: Request, res: Response) => {
     const user = User.build({ email, password })
     await user.save();
 
-    res.status(201).send(user)
+    //generating JWT
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, process.env.JWT_KEY!);
+
+
+    //store it on session object
+    req.session = {
+        jwt: userJwt
+    }
+
+    res.status(201).send(user);
 
 });
 
