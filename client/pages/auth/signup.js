6beb04a4362afb732ffd2ea6c046c56567383295
ttyt0416 <ttyt0416@gmail.com>
2021-03-24 16:36:22 +0900

    adding landing page(index.js) and custom Requesthook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,15 +1,43 @@
+import { useState } from 'react';
+import axios from 'axios';
+import useRequest from '../../hooks/use-request';
+
 export default () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    const onSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post('/api/users/signup', {
+                email, password
+            })
+            console.log(response.data);
+        } catch (err) {
+            setErrors(err.response.data.errors);
+        }
+    }
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <h1>Sign up</h1>
             <div className='form-group'>
                 <label>Email Address</label>
-                <input className='form-control' />
+                <input value={email} onChange={e => setEmail(e.target.value)} className='form-control' />
             </div>
             <div className='form-group'>
                 <label>Password</label>
-                <input type='password' className='form-control' />
+                <input value={password} onChange={e => setPassword(e.target.value)} type='password' className='form-control' />
             </div>
+            {errors.length > 0 && (
+            <div className='alert alert-danger'>
+                <ul className='my-0'>
+                    {errors.map(err => <li key={err.message}>{err.message}</li>)}
+                </ul>
+            </div>
+            )}
             <button className='btn btn-primary'>Sign Up</button>
         </form>
         
